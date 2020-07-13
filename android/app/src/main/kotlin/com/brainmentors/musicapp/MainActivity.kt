24583fec6004622e6c4fb1c76889b63310624b32
Akshay Makkar <akshaym1103@gmail.com>
2020-07-13 14:24:18 +0530

    Inital commit

---
+++ android/app/src/main/kotlin/com/brainmentors/musicapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.brainmentors.musicapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import 'homeScreen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Music App',
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    )
+  );
+}
